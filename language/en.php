f5433bafde5afcfc2faed2be02ba739986635fab
Archanaa08 <archanashekar.s@gmail.com>
2019-12-23 00:21:27 +0530

    Update en.php

--- language/en.php
+++ language/en.php
@@ -10,4 +10,4 @@ $lang["preview"] = "Preview";
 $lang["download-ready"] = "Video is ready for download";
 $lang["deleted-video"] = "This video has been deleted or hidden.";
 $lang["placeholder"] = "Enter Facebook video post URL";
-$lang["homepage-slogan"] = "You can change or remove this text. And you can add ad to below."; // You can use HTML tags
+$lang["homepage-slogan"] = ""; // You can use HTML tags
